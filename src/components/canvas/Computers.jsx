63c4e59cca9d6e9a21d366d29d1810050e84b702
Petra Bayu Pangestu <petrabayu19@gmail.com>
2024-02-10 22:40:33 +0700

    Adding 3d Object

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
-export default Computers
+  return (
+    <>
+      <mesh>
+        <hemisphereLight intensity={2} groundColor="blue" />
+        <pointLight intensity={3} />
+        <spotLight
+          position={[-20, 50, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize={1024}
+        />
+        f
+        <primitive
+          object={computer.scene}
+          scale={0.75}
+          position={[0, -3.25, -1.5]}
+          rotation={[-0.01, 0, -0.15]}
+        />
+      </mesh>
+    </>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          autoRotate={false}
+          autoRotateSpeed={1}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+// export default Computers;
+export default ComputersCanvas;
